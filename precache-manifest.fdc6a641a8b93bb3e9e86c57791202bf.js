a09f989a42b7210d26e8e916f25ed0a420a51254
amirbasheer <amirbashir880@gmail.com>
2019-02-06 14:39:31 +0500

    Updates

---
+++ precache-manifest.fdc6a641a8b93bb3e9e86c57791202bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b82781489c55695832c",
+    "url": "/todoRedux/static/js/runtime~main.3b827814.js"
+  },
+  {
+    "revision": "a9c55cc41a3e670b9180",
+    "url": "/todoRedux/static/js/main.a9c55cc4.chunk.js"
+  },
+  {
+    "revision": "738942581260f2360df0",
+    "url": "/todoRedux/static/js/1.73894258.chunk.js"
+  },
+  {
+    "revision": "a9c55cc41a3e670b9180",
+    "url": "/todoRedux/static/css/main.a047b0c3.chunk.css"
+  },
+  {
+    "revision": "e00386dbd1dd109e38811bc075a7953e",
+    "url": "/todoRedux/index.html"
+  }
+];
